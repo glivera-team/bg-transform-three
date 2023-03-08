c0e858cbb236f5dd097dfb38c070596483cad04d
lovesossa <qwertyerku5@gmail.com>
2023-03-08 11:11:49 +0200

    init

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,3 +1,12 @@
-const index = () => {};
+import Sketch from '../components/sketch';
+
+const index = () => {
+	const canvas = document.querySelector('#container');
+	if (canvas) {
+		const sketch = new Sketch({
+			dom: canvas,
+		});
+	}
+};
 
 export default index;
---
+++ src/static/shader/fragment.glsl
@@ -0,0 +1,17 @@
+uniform float time;
+uniform float progress;
+uniform sampler2D texture1;
+uniform vec4 resolution;
+varying vec2 vUv;
+varying vec3 vPosition;
+float PI = 3.141592653589793238;
+uniform sampler2D uTexture;
+
+void main()	{
+	vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+	vec4 color = texture2D(uTexture, newUV);
+
+	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+	// gl_FragColor = vec4(vUv,0.0,1.);
+	gl_FragColor = texture2D(uTexture, newUV - 0.02);
+}
